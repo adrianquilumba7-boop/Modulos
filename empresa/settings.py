6bf0aa6fc15b87501a4ca564d76d22b58d05ace7
adrianquilumba7-boop <adrianquilumba7@gmail.com>
2025-10-24 09:08:37 -0500

    vr

--- empresa/settings.py
+++ empresa/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'empresa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'usuarios' / 'templates'],  
+        'DIRS': [BASE_DIR  / 'templates'],  
         'APP_DIRS': True, 
         'OPTIONS': {
             'context_processors': [
